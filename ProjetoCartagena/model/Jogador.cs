ca81cb57fa77941a886de8b4bab337f547654b23
ygorkaique1999 <32522057+ygorkaique1999@users.noreply.github.com>
2019-03-21 20:58:40 -0300

    Modelos Jogador/Partida

---
+++ ProjetoCartagena/model/Jogador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCartagena
+{
+    class Jogador
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public string cor { get; set; }
+        public int posicao { get; set; }
+        public List<string> cartas { get; set; }
+    }
+}
---
+++ ProjetoCartagena/model/Partida.cs
@@ -0,0 +1,19 @@
+﻿using System;o
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCartagena
+{
+    class Partida
+    {
+        public List<Jogador> jogadores { get; set; }
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public Jogador vez { get; set; }
+        public string historico { get; set; }
+        public string tabuleiro { get; set; }
+    }
+}
